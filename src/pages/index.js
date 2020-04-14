53060dccf966a0ed5aae1156843031e7a4d86923
Merton <mertlansley@gmail.com>
2020-04-14 14:37:22 +0100

    Gutsy

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Index = () => {
   return (
     <>
-      <h1>EMF CI/CD Workshop</h1>
+      <h1>Mert's gutsy gatsby project</h1>
       <p>This is the project for our workshop on Gatsby Cloud and GitHub Actions</p>
       <p>Use this project for the workshop</p>
     </>
